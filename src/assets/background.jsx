01d1922cad6cbb830d4b5b4690b9778731ecabf4
YTMOONXD <89401142+YTMOONXD@users.noreply.github.com>
2025-06-19 07:27:10 +0300

    Add files via upload

---
+++ src/assets/background.jsx
@@ -0,0 +1,20 @@
+
+function Background(){
+    return(<>
+    <div className="nike-logo-continer"> 
+        <div className="contaner">
+                <div className="logo">
+                    <img src="public\pngfiles\logo.png" alt="" srcset="" />
+                </div>
+                <div className="jacket">
+                    <img src="public\pngfiles\jacket.png" alt="" srcset="" />
+                </div>
+                <div className="text">
+                    <h1>Follow for more</h1>
+                </div>
+        </div>
+    </div> 
+    </>)
+}
+
+export default Background
